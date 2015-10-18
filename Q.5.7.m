71fc410e7364a3edc8145115e2b0df4100a3f209
mateoaguirrer <mateo.aguirrer@autonoma.edu.co>
2015-10-17 21:10:19 -0500

    Q.5.7

---
+++ Q.5.7.m
@@ -0,0 +1,34 @@
+X = linspace(0,2*pi,20); % crea un vector de 20 elementos de 0 a 2pi
+Y1= sin(X); % crea un vector de 20 elementos Y1= sin(X) de 0 < X < 2pi
+Y2 =cos(X); % crea un vector de 20 elementos Y1= cos(X) de 0 < X < 2pi 
+Y3 =Y1+Y2;
+Y4 =Y1-Y2;
+plot (X,Y1,'K*:') % grafica sin(X) vs X con una linea(K) punteada(:) negra 
+                  % indicando los puntos con un asterisco(*)  
+hold on
+plot(X,Y2,'r--') % grafica cos(X) vs X con una linea(R) discontinua(--) roja   
+hold on
+plot(X,Y3,'b') % grafica [sin(X) + cos(X)] vs X con una linea(L) (azul)
+hold on
+plot(X,Y4,'g') % grafica [sin(X) - cos(X)] vs X con una linea(G) verde  
+hold off
+box off % suprime el cuadro de la figura
+title ('Funciones Trigonometricas con “Box Off”') % ver figura 5.64
+box on % enciende el cuadro de la figura
+title ('Funciones Trigonometricas con “Box On”') % ver figura 5.65
+grid on % enciende la cuadricula
+xlabel('Variable independiente X') % etiqueta el eje X
+ylabel('Varialbe dependiente Y') % etiqueta el eje Y 
+title ('Funciones trigonometricas con “Grid On”') % ver Figura 5.66
+grid off
+title ('Funciones trigonometricas con “Grid off”') % ver figura 5.67
+axis ([0 2*pi -2 1.5]) % establece los ejes 0 = X = -2pi y -2= Y = 1.5
+axis off % remueve los ejes
+axis on % enciende los ejes
+legend ('sin(X)','cos(X)','sin(X)+cos(X)','sin(X)-cos(X)','location','Northwest')
+title('Funciones trigonometricas con ejes y etiqueta arreglado') % ver figura 5.68
+gtext('sinX') % identifica cada curva con una cadena de texto
+gtext('cos(X)')
+gtext('sin(X)+cos(X)')
+gtext('sin(X)-cos(X)') % ver figura 5.69 donde las curvas estan identificadas con textos 
+ % graficar las 4 funciones usando steam, en separado subplot
