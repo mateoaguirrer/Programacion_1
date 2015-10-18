b4ff2ede373903af1bdab0358f7234ba488f78e2
mateoaguirrer <mateo.aguirrer@autonoma.edu.co>
2015-10-17 22:19:31 -0500

    P.5.6

--- P.5.6.m
+++ P.5.6.m
@@ -1,11 +1,11 @@
 X = linspace(0,2*pi,20);
 Y1 = sin(X).*sin(X);
-plot(X,Y1,'b--v')
+plot(X,Y1,'b--')
 hold on
 Y2 = cos(X).*cos(X);
-plot(X,Y2,'rv:')
+plot(X,Y2,'r:')
 hold on
 Y3 = (sin(X).*sin(X))+(cos(X).*cos(X));
-plot(X,Y3,'kv')
+plot(X,Y3,'k')
 axis([0 6.2832 0 1.5])
 legend('sin^2(X)','cos^2(X)','sin^2(X)+cos^2(X)')
